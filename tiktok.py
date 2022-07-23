5088c58018673702deb58c7f9a088e514b53c1d1
Elizabeth Kok <elizabethkok_97@hotmail.com>
2022-07-24 06:56:19 +0900

    added new function for tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -3,7 +3,7 @@ from TikTokApi import TikTokApi as tiktok
 # Import JSON for export of data
 import json
 # Import data processing helper
-from helpers import process_results 
+from helpers import process_results
 # Import pandas to create dataframes
 import pandas as pd
 # Import sys dependency to extract command line arguments
@@ -22,6 +22,17 @@ def get_data(hashtag):
     df = pd.DataFrame.from_dict(flattened_data, orient='index')
     df.to_csv('tiktokdata.csv', index=False)
 
-if __name__ == '__main__':
-    get_data(sys.argv[1])
+def get_json(hashtag):
+    # Get cookie data
+    verifyFp = "verify_kx2ee558_BH6fvQVi_cXHF_4lfK_Bimg_hH0lYMCV6Vm6"
+    # Setup instance
+    api = tiktok.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
+    # Get data by hashtag
+    trending = api.by_hashtag(hashtag)
+    with open("exporttry.json",'w') as f:
+        json.dump(trending,f)
 
+get_json("python")
+
+# if __name__ == '__main__':
+#     get_data(sys.argv[1])
